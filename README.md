78d44abee8b9c30fcb88e0d85eca3fb68a17615b
nav <nav_garcha@hotmail.co.uk>
2011-12-30 13:59:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
-# README for a newly created project.
+# jQuery Facebook Plugin
 
-There are a couple of things you should do first, before you can use all of Git's power:
+Plugin allows for quick and easy setup for the Facebook JS SDK within the jQuery wrapper.
+Look at source for config options till I decide to write up a proper guide. ;)
 
-  * Add a remote to this project: in the Cloud9 IDE command line, you can execute the following commands
-    `git remote add [remote name] [remote url (eg. 'git@github.com:/ajaxorg/node_chat')]` [Enter]
-  * Create new files inside your project
-  * Add them to to Git by executing the following command
-    `git add [file1, file2, file3, ...]` [Enter]
-  * Create a commit which can be pushed to the remote you just added
-    `git commit -m 'added new files'` [Enter]
-  * Push the commit the remote
-    `git push [remote name] master` [Enter]
+Exposes useful information such as:
+    `jQuery.facebook.inCanvas` - Is page within FB Canvas.
+    `jQuery.facebook.isReady` - Can access window.FB.
 
-That's it! If this doesn't work for you, please visit the excellent resources from [Github.com](http://help.github.com) and the [Pro Git](http://http://progit.org/book/) book.
-If you can't find your answers there, feel free to ask us via Twitter (@cloud9ide), [mailing list](groups.google.com/group/cloud9-ide) or IRC (#cloud9ide on freenode).
+Provides utility functions: 
+    `jQuery.facebook.ensure(callback [, waitForAuth])` - Execute code only once FB has been included and initalised, also have the option to wait for user to be authenticaed before executing. 
+    `jQuery.facebook.context(canvasCallback, normalCallback)` - Execute certain code depending on whether your code is being run within the FB Canvas or normally.
 
-Happy coding!
+
+## Example Usage
+
+    $.facebook(1234567890);
+    
+    $.facebook(1234567890, {    
+        initQueue: [function() {         
+            console.log('1st');    
+        }, function() {         
+            console.log('2nd');    
+        }, function() {         
+            console.log('3rd');    
+        }]
+    });
+
+Version: 0.1
+Author: Navkaran Garcha
+Mail: nav_garcha@hotmail.co.uk
+Twitter: @Nav_Garcha
---
+++ jquery.facebook.js
@@ -0,0 +1,145 @@
+/*
+* jQuery Facebook - plugin to assist with the developement
+* of Facebook applications using the Facebook JS SDK.
+*
+* Copyright (c) 2011-2012 Navkaran Garcha <nav_garcha@hotmail.co.uk>
+*
+* Licensed under the MIT license:
+*   http://www.opensource.org/licenses/mit-license.php
+*
+* Version: 0.1
+* Dependencies: jQuery
+* Usage: jQuery.facebook( fbAppId [, settings] )
+*
+*/
+
+(function($, window, document, undefined) {
+    // Private variables
+    var _settings,
+        _isAuthed,
+        _isInitd,
+        _inCanvas,
+        _eventTarget;
+    
+    // Plugin declaration
+    $.facebook = function(appId, options) {
+        // Ensure FB appId is passed and SDK isnt already initiated
+        if(!appId || _isInitd) return;
+        
+        _inCanvas = window.top != window;
+        _settings = $.extend({}, $.facebook.settings, options);
+        _eventTarget = $($.facebook.settings.eventTarget);
+        
+        _setupPage(function() {
+            _initSdk($.extend(_settings.sdkInitOpts, {
+                appId: appId    // Add the passed in appId to the config object for init
+            }));
+        });
+        
+        // Successfully initiated
+        return true;
+    };
+    
+    // Expose to public 
+    $.extend($.facebook, {
+        // Default options for plugin
+        settings: {
+            staticPageSize: false,
+            eventTarget: document,
+            userLogged: undefined,
+            initQueue: [],
+            sdkInitOpts: {
+                status: true,
+                cookie: true,
+                xfbml: true
+            },
+            onLogout: function() { window.location.reload(); },
+            onLogin: function() { window.location.reload(); }
+        },
+        // Execute code only once FB is ready, with the option to wait till auth
+        ensure: function(callback, waitForAuth) {
+            if(!_isInitd || (waitForAuth && !_isAuthed)) {
+                setTimeout(function() {
+                    $.facebook.ensure(callback, waitForAuth);
+                }, 100);
+            } else if(callback) {
+                callback();
+            }
+        },
+        // Context check to execute code depending on whether site is in FB Canvas or not
+        context: function(canvasCallback, normalCallback) {
+			if(_inCanvas && typeof canvasCallback === 'function') {
+				canvasCallback();
+			} else if(!_inCanvas && typeof normalCallback === 'function') {
+				normalCallback();
+			}
+		}
+    });
+    
+    // Private methods
+    function _setupPage(loadCallback) {
+        // Insert the required DOM elm
+        $('body').append('<div id="fb-root" />');
+        
+        // Asynchronously load in the JS SDK
+        $.getScript('//connect.facebook.net/en_US/all.js', loadCallback);
+    }
+    
+    function _initSdk(settings) {
+        FB.init(settings);
+        FB.Canvas.scrollTo(0, 0);   // Ensure we are at the top of page on load
+        FB.getLoginStatus(_handleUserStateChange);
+        
+        if(!_settings.staticPageSize) {
+            FB.Canvas.setAutoGrow();
+        }
+        
+        _isInitd = true;
+        
+        _initEvents();
+        _processInitQueue();
+        _exposeYourself();
+        
+        _eventTarget.trigger('ready.facebook');
+    }
+    
+    function _processInitQueue() {
+        while(_settings.initQueue.length) {
+            var callback = _settings.initQueue.shift();
+            if(typeof callback === 'function') {
+                callback();
+            }
+        }
+        
+        _eventTarget.trigger('clearqueue.facebook');
+    }
+    
+    function _initEvents() {
+        FB.Event.subscribe('auth.statusChange', _handleUserStateChange);
+        
+        if(_settings.userLogged !== undefined) {
+            _eventTarget.bind('userlogout.facebook', _settings.onLogout);
+            _eventTarget.bind('userlogin.facebook', _settings.onLogin);
+        }
+    }
+    
+    function _handleUserStateChange(state) {
+        _isAuthed = !!state.authResponse;
+        
+        if(_settings.userLogged !== undefined) {
+            if(!_isAuthed && _settings.userLogged) {
+                _eventTarget.trigger('userlogout.facebook');
+            } else if(_isAuthed && !_settings.userLogged) {
+                _eventTarget.trigger('userlogin.facebook');
+            }
+        }
+    }
+    
+    function _exposeYourself() {
+        // Exposing these can be useful
+        $.extend($.facebook, {
+            inCanvas: _inCanvas,
+            isReady: _isInitd
+        });
+    }
+})(jQuery, window, document);
